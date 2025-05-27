af1ca6e63d1aabfea1ea250db46cab662e7c874c
soheeyun <sul831@naver.com>
2025-05-27 16:22:32 +0900

    feat: 최초세팅

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-blue-100">
+      <h1 className="text-4xl font-bold text-blue-600">Hello Tailwind + React!</h1>
+    </div>
+  );
+}
+
+export default App;
